fffef0edb2be6951e1e42bfe7c8d6c02dff5c9b7
Zi Ye <18612319597@163.com>
2020-01-29 17:33:04 +0800

    [feature] statement: for

---
+++ statement/for.dart
@@ -0,0 +1,10 @@
+void main() {
+  var list = [1,2,3,4,5];
+  for(var i = 0; i < list.length; i++) {
+    print(list[i]);
+  }
+  print('----------');
+  for(var item in list) {
+    print(item);
+  }
+}
