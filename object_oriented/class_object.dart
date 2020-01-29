69c3b214657c7bfebc84c4b86704aaec6bf0d09b
Zi Ye <18612319597@163.com>
2020-01-29 21:26:18 +0800

    [feature] object_oriented: class_object

---
+++ object_oriented/class_object.dart
@@ -0,0 +1,10 @@
+import 'person.dart';
+
+void main() {
+  var person = new Person();
+  person.name = 'Zi Ye';
+  person.age = 23;
+  print(person.name);
+  print(person.address);
+  person.work();
+}
---
+++ object_oriented/person.dart
@@ -0,0 +1,9 @@
+class Person {
+  String name;
+  int age;
+  final String address = 'China';
+
+  void work() {
+    print('name is ${name}, age is ${age}, TA is working');
+  }
+}
