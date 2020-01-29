67ff4cfa0b33fcf52eefb1fbcf4a2e98afdcf45b
Zi Ye <18612319597@163.com>
2020-01-29 17:03:24 +0800

    [feature] operator: arithmetic

---
+++ operator/arithmetic.dart
@@ -0,0 +1,17 @@
+void main() {
+  int a = 10;
+  int b = 2;
+
+  print(a + b); // 12
+  print(a - b); // 8
+  print(a * b); // 20
+  print(a / b); // 5.0
+  print(a ~/ b); // 5
+  print(a % b); // 0
+
+  print(a++); // 10
+  print(++a); // 12
+
+  print(a--); // 12
+  print(--a); // 10
+}
